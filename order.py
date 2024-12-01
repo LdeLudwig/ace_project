41c4fff9e21dc82a03e88f48126c0351f2150ffc
ludwig <lucasx@elligence.com.br>
2024-12-01 20:33:01 -0300

    hotfix to connect with esp32

---
+++ order.py
@@ -0,0 +1,91 @@
+import gspread
+import time
+import json
+import socket
+from datetime import datetime
+from google.oauth2.service_account import Credentials
+
+# Configutação do socket client dos pedidos
+DESTINATION_ADDR = '0.0.0.0'
+SOURCE_PORT, DESTINATION_PORT = 9090, 8089
+
+orders_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+orders_client_socket.bind(('localhost',SOURCE_PORT))
+orders_client_socket.connect((DESTINATION_ADDR, DESTINATION_PORT))
+
+# Configuração das credenciais e inicialização do cliente gspread
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets"
+]
+creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+client = gspread.authorize(creds)
+
+# Acessa a planilha e a worksheet específica
+sheet_id = "1Kw-RtdJLl-Tk8D5OdWT1vCTw9eyZEQcTHjg-4z5R0-M"
+orders_sheet = client.open_by_key(sheet_id)
+worksheet = orders_sheet.worksheet("Pedidos")
+
+# Colore a linha do pedido
+def color_cells(row_index, status):
+    if status:
+        worksheet.format(f'A{row_index}:C{row_index}', {
+            'backgroundColor':{
+                'red': 0.8,
+                'green': 1.0,
+                'blue': 0.8
+            }
+        })
+    else:    
+        worksheet.format(f'A{row_index}:C{row_index}', {
+            'backgroundColor':{
+                'red': 1.0,
+                'green': 0.8,
+                'blue': 0.8
+            }
+        })
+        
+# Transforma array em objeto json    
+def data_transform(orders_info):
+    # Adiciona a data do pedido
+    date = datetime.now().strftime("%d/%m/%Y")
+    
+    order_dict = {
+        'date': date,
+        'product_num': orders_info[0],
+        'qty': int(orders_info[1]),
+        'order_num': int(orders_info[2]),
+    }
+    order_json = json.dumps(order_dict)
+    print(order_json)
+    return order_json
+    
+# Verifica a planilha de pedidos a cada 1 segundo 
+def watch_changes():
+    # Pega os dados iniciais e cria um dicionário para rastrear mudanças
+    previous_data = {i: row for i, row in enumerate(worksheet.get("C4:C50"), start=4)}
+    
+    while True:
+        # Obtém o estado atual das células do intervalo
+        current_data = {i: row for i, row in enumerate(worksheet.get("C4:C50"), start=4)}
+        
+        # Itera sobre cada linha para detectar alterações
+        for row_index in current_data:
+            prev = previous_data.get(row_index)
+            curr = current_data.get(row_index)
+            
+            if prev != curr:
+                # Se houver mudança, imprime e aplica destaque
+                new_row_data = worksheet.row_values(row_index)
+                print(f"MUDOU na linha {row_index}:", new_row_data)
+                order_json = data_transform(new_row_data)
+                orders_client_socket.send(bytes(order_json,'UTF-8'))
+                
+                color_cells(row_index)
+        
+        # Atualiza `previous_data` com o estado atual completo
+        previous_data = current_data.copy()
+        
+        # Pausa antes da próxima verificação
+        time.sleep(1)
+
+watch_changes()
---
+++ storage.py
@@ -0,0 +1,130 @@
+import gspread
+import json
+import socket
+from threading import Thread
+from google.oauth2.service_account import Credentials
+
+# Configuração do socket server
+SERVER_ADDR = '0.0.0.0'
+SERVER_PORT = 8089
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.settimeout(None)
+server_socket.bind((SERVER_ADDR, SERVER_PORT))
+server_socket.listen(5)
+
+# Configuração das credenciais e inicialização do cliente gspread
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets"
+]
+creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+client = gspread.authorize(creds)
+
+# Acessa a planilha e a worksheet específica
+sheet_id = '10XiI62jg7ElYyf1AWhK_bp41C2zeU7-U_D-lTlQHMYY'
+storage_sheet = client.open_by_key(sheet_id)
+worksheet = storage_sheet.worksheet("Estoque")
+
+def get_last_row():
+    all_data = worksheet.get_all_values()
+    for i in range(len(all_data) - 1, 3, -1):  # Itera de baixo para cima, começando na linha 4
+        if any(all_data[i]):  # Se a linha não estiver vazia
+            return i + 1  # Retorna índice 1-based
+    return 4  # Retorna 4 se não encontrar nenhuma linha preenchida
+
+
+def handle_orders(json_object, connection):
+    try:
+        date = json_object['date']
+        product_num = json_object['product_num']
+        qty_requested = json_object['qty']
+        order_num = json_object['order_num']
+
+        # Obtem a última linha preenchida do estoque
+        last_row = get_last_row()
+        last_data = worksheet.row_values(last_row)
+        headers = worksheet.row_values(4)  # Cabeçalhos esperados na linha 4
+        product_key = f"Quant. total prod. {product_num}"
+
+        # Mapeia índice da coluna do produto
+        if product_key in headers:
+            product_col = headers.index(product_key)
+        else:
+            print(f"Erro: Produto '{product_num}' não encontrado na planilha.")
+            connection.send(json.dumps({"status": False, "message":f"Produto {product_num} não encontrado"}).encode('utf-8'))
+            return
+
+        # Verifica saldo atual do produto
+        current_stock = int(last_data[product_col]) if product_col < len(last_data) and last_data[product_col] else 0
+        print(f"Estoque atual do produto {product_num}: {current_stock}")
+
+        # Valida estoque
+        if current_stock >= qty_requested:
+            connection.send(json.dumps({"status": True,"message":f"Pedido {order_num} processado com sucesso!"}).encode('utf-8'))
+
+            new_stock = current_stock - qty_requested
+
+            # Atualiza planilha
+            new_row = [date] + [""] * (product_col - 1) + [new_stock] + [""] * (len(headers) - product_col - 1)
+            worksheet.append_row(new_row)
+            print(f"Pedido #{order_num} processado com sucesso! Estoque atualizado para {new_stock}.")
+        else:
+            print(f"Erro: Estoque insuficiente para o produto {product_num}. Pedido #{order_num} recusado.")
+            connection.send(json.dumps({"status": False, "message":f"Estoque insuficiente para o produto {product_num}"}).encode('utf-8'))
+    except Exception as e:
+        print(f"Erro ao processar pedido: {str(e)}")
+    return True
+    
+    
+    
+def sending_order_to_manipulator(json_object):
+    """ 
+    
+    Adicionar lógica para envio do pedido para o robô
+    
+    """
+    return 1
+
+
+def process_connection(connection, addr):
+    try:
+        while True:
+            buf = connection.recv(3000)
+            if not buf:
+                print(f"Conexão encerrada com {addr}")
+                break
+            
+            print(addr)
+            if addr[1] == 9090:
+                try:
+                    # Decodifica os dados recebidos e converte para JSON
+                    data = buf.decode('utf-8')
+                    json_object = json.loads(data)
+                    order = handle_orders(json_object, connection)
+                    
+                    if order:
+                        sending_order_to_manipulator(json_object)
+                except json.JSONDecodeError:
+                    print("Erro: Dados recebidos não estão no formato JSON:", buf.decode('utf-8'))
+                    connection.send(json.dumps({"status": False, "message": "Formato inválido"}).encode('utf-8'))
+            else:
+                data = buf.decode('utf-8')
+                print(f"Conexão recebida de outro tipo: {addr}")
+                #connection.send("Conexão aceita, mas lógica ainda não definida.".encode('utf-8'))
+                
+                
+    except Exception as e:
+        print(f"Erro na conexão com {addr}: {str(e)}")
+    finally:
+        connection.close()
+
+
+def accept_connections():
+    # Servidor esperando por conexões        
+    while True:
+        connection, addr = server_socket.accept()
+        print(f"{addr}")
+        thread = Thread(target=process_connection, args=(connection, addr))
+        thread.start()
+
+
+accept_connections()
